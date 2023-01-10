0133d602d0190505426e99520adf70156c16d97d
SchopperAle <alexander.schopper@htl-wels.at>
2023-01-10 10:49:46 +0100

    DB

--- index.js
+++ index.js
@@ -2,63 +2,55 @@
 let sqlite = require("sqlite3");
 let express = require("express");
 let fs = require("fs");
+let bodyParser = require('body-parser');
 
 // Einstellungen
 let settings = JSON.parse(fs.readFileSync("./settings.json")+"");
 
 // SQL-Teil
-let dblocation = settings.databaseLocation;
-let db = new sqlite.Database(dblocation, sqlite.OPEN_READWRITE, (err)=> {
-    if(err && err.code == "SQLITE_CANTOPEN"){
-        createDB();
-        console.log("Datenbank wurde erstellt. Bitte starte den Server erneut.")
-        process.exit(0);
-    }
-    else if(err){
+let dbname = settings.databaseName;
+let db = new sqlite.Database("./backend/"+dbname, (err)=> {
+    
+    if(err){
         console.log(err);
         process.exit(1);
     }
+
+    createDB();
 });
 
 function createDB(){
-    let db = new sqlite.Database(dblocation, (err) => {
-        if(err){
-            console.log(err);
-            process.exit(2);
-        }
-    });
-
     db.exec(`
-        create TABLE Mitarbeiter (
-            id int primary key AUTOINCREMENT,
+        create TABLE IF NOT EXISTS Mitarbeiter(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
             name varchar(255)
         );
 
-        create TABLE Board (
-            id int primary key AUTOINCREMENT,
+        create TABLE IF NOT EXISTS Board(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
             name varchar(32),
             beschreibung text
         );
 
-        create TABLE Aufgabe (
-            id int primary key AUTOINCREMENT,
+        create TABLE IF NOT EXISTS Aufgabe(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
             name varchar(255),
             beschreibung text,
             mitarbeiter int,
             foreign key (mitarbeiter) references Mitarbeiter(id)
         );
 
-        create TABLE Board_Mitarbeiter (
-            board int not null,
-            mitarbeiter int not null,
+        create TABLE IF NOT EXISTS Board_Mitarbeiter(
+            board INTEGER not null,
+            mitarbeiter INTEGER not null,
             foreign key (board) references Board(id),
             foreign key (mitarbeiter) references Mitarbeiter(id),
             constraint pkey PRIMARY KEY (board, mitarbeiter)
         );
 
-        create TABLE Aufgabe_Board (
-            aufgabe int not null,
-            board int not null,
+        create TABLE IF NOT EXISTS Aufgabe_Board(
+            aufgabe INTEGER not null,
+            board INTEGER not null,
             foreign key (aufgabe) references Aufgabe(id),
             foreign key (board) references Board(id),
             constraint pkey PRIMARY KEY (aufgabe, board)
@@ -70,11 +62,30 @@ function createDB(){
 let port = settings.port;
 let app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.use("/web", express.static("web"));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname+"/web/index.html");
 });
+
+app.post("/createMitarbeiter", (req, res) => {
+    db.exec(`
+        insert into Mitarbeiter (name) values (`+ req.body.name +`);
+    `);
+
+    db.all("select * from Mitarbeiter;", (err, rows) => {
+        if(err){
+            console.log(err);
+        }
+        rows.forEach((row) => {
+            console.log(row);
+        });
+    });
+
+    res.send("Created "+req.body.name);
+})
 
 app.listen(port, () => {
     console.log("Started.", "Listening to port "+port);
