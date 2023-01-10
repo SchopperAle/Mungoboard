98d51c6996c9ea827a2f3e2e9a878024d355325e
SchopperAle <alexander.schopper@htl-wels.at>
2023-01-10 09:34:13 +0100

    A wengal basic Zeigs

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+let sqlite = require("sqlite3");
+let express = require("express");
+let fs = require("fs");
 
+let settings = JSON.parse(fs.readFileSync("./settings.json")+"");
+let port = settings.port;
+
+let app = express();
+
+app.use("/web", express.static("web"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname+"/web/index.html");
+});
+
+app.listen(port, () => {
+    console.log("Started.", "Listening to port "+port);
+})
