3d571ae454eb5b1b8a935f4ee7896c49c625e1be
SchopperAle <alexander.schopper@htl-wels.at>
2023-02-07 10:09:05 +0100

    Dings

--- web/index.js
+++ web/index.js
@@ -4,12 +4,14 @@ if(localStorage.getItem("id") != undefined){
     .done((data) => {
         let txt = "";
 
+        // Boards anzeigen
         data.data.forEach((val) => {
             txt+="<div id='"+val.board+"' class='board'>"+val.name+"</div>";
         });
 
         $("#boards").html(txt);
 
+        // Boards klickbar machen
         data.data.forEach((val) => {
             $("#"+val.board).on("click", () => {
                 window.location.href = "/page/board/"+val.board;
