cef7faf901356bc30d607c9933bc9608854de082
SchopperAle <alexander.schopper@htl-wels.at>
2023-06-13 09:16:41 +0200

    Fixed Login+Register

--- web/user/login.js
+++ web/user/login.js
@@ -14,34 +14,18 @@ function login(){
         });
 }
 
-let savecount = 0;
 // Register
 function register(){
-    savecount++;
-    if(savecount <= 1){
-        let name = $("#name").val();
-        let passwort = $("#passwort").val();
-        $.ajax({url:"/createMitarbeiter", type:"POST", data:{name:name, passwort:passwort}})
-        .done((data) => {console.log(data);
-            if(data?.id == undefined){  // Fehler
-                alert(data);
-                $("#lgbt").attr("id", "loginButton");
-                savecount = 3;
-                setTimeout(() => savecount = 0, 1000);
-            }else {
-                window.location.href = "/page/profile";
-            }
-        }).fail((data) => { // Fehler
-            alert(data.responseText);
-            savecount = 3;
-            setTimeout(() => savecount = 0, 1000);
-        });
-    }
+    let name = $("#name").val();
+    let passwort = $("#passwort").val();
+    $.ajax({url:"/createMitarbeiter", type:"POST", data:{name:name, passwort:passwort}})
+    .done((data) => {console.log(data);
+        if(data?.id == undefined){  // Fehler
+            alert(data);
+        }else {
+            window.location.href = "/page/profile";
+        }
+    }).fail((data) => { // Fehler
+        alert(data.responseText);
+    });
 }
-
-// Event-Listener -> Führt bei Enter-Klick zum Login
-addEventListener("keypress", (ev) => {
-    if(ev.key == "Enter"){
-        $("#loginButton").trigger("click").attr("id", "lgbt");
-    }
-})
